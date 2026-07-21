8ca8094f06ffdf823344cbffdf3bbc0be773c8d5
Ayushsingh998 <ayushsingh7625@gmail.com>
2026-07-21 18:59:19 +0530

    pipeline creating

--- src/utils.py
+++ src/utils.py
@@ -52,3 +52,17 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys) 
+
+def load_object(file_path):
+    try:
+        print("Loading:", file_path)
+
+        with open(file_path, "rb") as file_obj:
+            obj = pickle.load(file_obj)
+
+        print("Loaded Successfully:", file_path)
+        return obj
+
+    except Exception as e:
+        print("ERROR:", e)
+        raise CustomException(e, sys)
